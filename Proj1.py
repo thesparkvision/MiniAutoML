ae73fa07edcfe270a9b28d43a84f1047806f9b06
thesparkvision <aman15.pandya@gmail.com>
2020-06-14 09:17:50 +0530

    added browsing file feature

--- Proj1.py
+++ Proj1.py
@@ -4,22 +4,28 @@ import pandas as pd
 
 def sidebr():
     st.sidebar.title("Welcome to A^2-AutoML")
-    with st.sidebar.file_input() as input:
-        if input == None:
+    file=st.sidebar.file_uploader("Choose a file",type="csv")
+    if file is None:
             st.sidebar.warning("No file selected")
-        else:
-            file_contents = input.read()
+    else:
+        df=pd.read_csv(file)
+        print(type(df))
+        print(df.head())
             
-    model = st.sidebar.selectbox("Choose your model: ",["SVM","LinearRegression","LogisticRegression","DescionTree"])
+    model = st.sidebar.selectbox("Choose your model: ",["SVM","LinearRegression","LogisticRegression","DecisionTree"])
     if model == "SVM":
-        Svm_Model()
+        svm_model()
     elif model == "LinearRegression":
-        Linear_Reg()
+        linear_reg()
     elif model == "LogisticRegression":
-        Logistic_Reg()
-    elif model == "DescisionTree":
-        Descision_Tree()
+        logistic_reg()
+    elif model == "side":
+        decision_tree()
 
+def main():
+    sidebr()
 
+if __name__=="__main__":
+    main()
 
     
